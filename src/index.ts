80ea455373815ff75944891dfa06115031475139
Patrick Bacon-Blaber <pablaber225@gmail.com>
2020-12-22 13:12:28 -0500

    placeholder

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello')
+const superagent = require('superagent');
+const request = superagent.agent();
+
+interface AuthenticateOptions {
+  username: string
+  password: string
+}
+
+function authenticate(options: AuthenticateOptions) {
+
+}
+
+async function instructor() {
+  const res = await request.get('https://api.onepeloton.com/api/instructor');
+  return res;
+}
+
+console.log('poopy');
